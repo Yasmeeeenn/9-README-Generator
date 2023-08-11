3ac9f3203a2165c741251f3c41be607a3fa28bb8
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-08-10 19:22:12 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,11 +7,50 @@ const questions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is the title of your README?'
+        message: 'Enter the title of your README.'
     },
     {
-
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your application.'
+    },
+    {
+        type: 'input',
+        name:'installation',
+        message: 'Enter installation instruction.',
+    },
+    {
+        type: 'input',
+        name:'usage',
+        message: 'Enter usage information.',
+    },
+    {
+        type: 'input',
+        name:'license',
+        message: 'Chose a license.',
+        choices: ['MIT', 'Apache', 'GPL'],
+    },
+    {
+        type: 'input',
+        name:'contribution',
+        message: 'Enter contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name:'test',
+        message: 'Enter test instruction.',
+    },
+    {
+        type: 'input',
+        name:'github',
+        message: 'Enter your GitHub username.',
+    },
+    {
+        type: 'input',
+        name:'email',
+        message: 'Enter your email address.',
     }
+    
 ];
 
 // TODO: Create a function to write README file
