b7356a772d455ef57752e7733d798423c8914fa9
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-08-10 19:58:14 -0500

    function to return license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,6 +10,10 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license) {
+    return `[License](#license)`;
+  }
+  return '';
 }
 
 // TODO: Create a function that returns the license section of README
